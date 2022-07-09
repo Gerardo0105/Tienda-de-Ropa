3eaf4f9be384e533f200125dd5563b65a72fb78b
Gerardo0105 <gchaves0587@gmail.com>
2022-07-08 21:19:06 -0600

    Creacion de comandos para reducir código

--- cypress-e2e/cypress/e2e/Search.cy.js
+++ cypress-e2e/cypress/e2e/Search.cy.js
@@ -3,12 +3,15 @@ describe('Search Elements', ()=>{
         cy.visit('/')
     })
     it('Búsqueda de elementos con resultados',()=>{
-        cy.fixture('PaginaInicial').then((index)=>{
-            cy.get(index.search).type('dress');
-            cy.get(index.searchbutton).click();  
-        })
+        cy.search('dress');
         cy.fixture('Resultados').then((Resultados)=>{
             cy.get(Resultados.title).should('contain','dress'); 
+        })
+    })
+    it('Búsqueda de elementos sin resultado',()=>{
+        cy.search('qwarty');
+        cy.fixture('Resultados').then((Resultados)=>{
+            cy.get(Resultados.Alerta).should('contain','No results were found for your search'); 
         })
     })
 })
